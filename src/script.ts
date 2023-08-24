8cc72f80d1904d5c25f94fb536d3316d097857ca
Ilya Zhidkov <xkelxmc@yandex.ru>
2023-08-24 02:56:02 +0000

    initial

---
+++ src/script.ts
@@ -0,0 +1,4 @@
+const lootBucketsUrl = 'https://cdn.cany.link/_0/javelindata_lootbuckets.json';
+const lootTablesUrl = 'https://cdn.cany.link/_0/javelindata_loottables.json';
+
+console.log('hello world!')
